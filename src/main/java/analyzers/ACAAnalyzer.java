b559e3bea2448ecc75ff663c78363516e4707ef4
anhnguyent93 <anh.nguyent93@gmail.com>
2018-10-26 14:39:49 -0500

    Update ACA Analyzer

--- src/main/java/analyzers/ACAAnalyzer.java
+++ src/main/java/analyzers/ACAAnalyzer.java
@@ -1,8 +1,88 @@
 package analyzers;
 
+import components.History;
+import components.Operation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by DJ Yuhn on 10/10/2018
+ * Modified by Anh Nguyen on 10/23/2018
  */
 public class ACAAnalyzer {
 
+    private History history;
+
+    public ACAAnalyzer(History history) {
+        this.history = history;
+    }
+
+    public History getHistory() {
+        return history;
+    }
+
+    public void setHistory(History history) {
+        this.history = history;
+    }
+
+    public String ACAChecking() {
+        Map<Integer, List<Integer>> writeSet = new HashMap<>();
+        List<Integer> commitSet = new ArrayList<>();
+        boolean flag = false;
+
+        if (history.getTxnsList().size() == 1)
+            return ("History is not ACA because it only has 1 transaction");
+
+        for (Operation op : history.getHistory()) {
+            if (op.getOperation() == 'w') {
+                if (writeSet.containsKey(op.getDataItem())) {
+                    List<Integer> listWrittenToDataItem = writeSet.get(op.getDataItem());
+                    listWrittenToDataItem.add(op.getTxnID());
+                } else {
+                    List<Integer> listWrittenToDataItem = new ArrayList<>();
+                    listWrittenToDataItem.add(op.getTxnID());
+                    writeSet.put(op.getDataItem(), listWrittenToDataItem);
+                }
+            }
+
+            if (op.getOperation() == 'r') {
+                if (writeSet.containsKey(op.getDataItem())) {
+                    List<Integer> listWrittenToDataItem = writeSet.get(op.getDataItem());
+                    if (!listWrittenToDataItem.isEmpty()) {
+                        int lastTxnWrittenToDataItem = listWrittenToDataItem.get(listWrittenToDataItem.size() - 1);
+                        if (lastTxnWrittenToDataItem != op.getTxnID()) {
+                            if (!commitSet.contains(lastTxnWrittenToDataItem)) {
+                                return ("History is not ACA because T" + op.getTxnID() + " reads the data item "
+                                        + op.getDataItem() + " from T" + lastTxnWrittenToDataItem
+                                        + " before T" + lastTxnWrittenToDataItem + " commit");
+                            } else {
+                                flag = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (op.getOperation() == 'c') {
+                commitSet.add(op.getTxnID());
+            }
+
+            if (op.getOperation() == 'a') {
+                for (Map.Entry<Integer, List<Integer>> entry : writeSet.entrySet()) {
+                    List<Integer> listWrittenToDataItem = entry.getValue();
+                    if (!listWrittenToDataItem.isEmpty()) {
+                        listWrittenToDataItem.remove(op.getTxnID());
+                    }
+                }
+            }
+        }
+
+        if (flag)
+            return ("History is ACA because all transaction reads those values that are written by a committed transaction");
+        else
+            return ("History is not ACA because no transaction reads from other transactions");
+    }
 }
